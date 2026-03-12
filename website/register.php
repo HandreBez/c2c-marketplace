2474d5822054915a61c6818ff2fb10e953608d56
Handre Bezuidenhout <you@example.com>
2026-03-12 20:58:23 +0200

    Added user registration system

---
+++ website/register.php
@@ -0,0 +1,52 @@
+<?php
+include "db.php";
+
+if(isset($_POST['register'])){
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO users (name, email, password)
+VALUES ('$name','$email','$password')";
+
+if($conn->query($sql) === TRUE){
+    echo "Account created successfully";
+} else {
+    echo "Error: " . $conn->error;
+}
+
+}
+?>
+
+<?php
+include "db.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Register</title>
+</head>
+
+<body>
+
+<h2>Create Account</h2>
+
+<form method="POST">
+
+Name:<br>
+<input type="text" name="name" required><br><br>
+
+Email:<br>
+<input type="email" name="email" required><br><br>
+
+Password:<br>
+<input type="password" name="password" required><br><br>
+
+<button type="submit" name="register">Register</button>
+
+</form>
+
+</body>
+</html>
